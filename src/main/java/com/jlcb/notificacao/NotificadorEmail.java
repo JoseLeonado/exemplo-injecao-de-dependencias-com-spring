056680363d4773c1d0d3006e38bafe7512598d32
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2022-04-01 23:34:05 -0300

    Conhecendo os pontos de injeção e a anotação Autowired

--- src/main/java/com/jlcb/notificacao/NotificadorEmail.java
+++ src/main/java/com/jlcb/notificacao/NotificadorEmail.java
@@ -1,28 +1,16 @@
 package com.jlcb.notificacao;
 
+import org.springframework.stereotype.Component;
+
 import com.jlcb.model.Cliente;
 
+@Component
 public class NotificadorEmail implements Notificador {
 	
-	private boolean caixaAlta;
-	private String hostServidorSmtp;
-	
-	public NotificadorEmail(String hostServidorSmtp) {
-		this.hostServidorSmtp = hostServidorSmtp;
-	}
-
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		if (caixaAlta) {
-			mensagem = mensagem.toUpperCase();
-		}
-		
-		System.out.printf("Notificando %s através do e-mail %s usando SMTP %s: %s\n", cliente.getNome(), cliente.getEmail(), hostServidorSmtp, 
-				mensagem);
+		System.out.printf("Notificando %s através do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
 	}
 	
-	public void setCaixaAlta(boolean caixaAlta) {
-		this.caixaAlta = caixaAlta;
-	}
 
 }
--- src/main/java/com/jlcb/service/AtivacaoClienteService.java
+++ src/main/java/com/jlcb/service/AtivacaoClienteService.java
@@ -1,5 +1,6 @@
 package com.jlcb.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.jlcb.model.Cliente;
@@ -8,15 +9,25 @@ import com.jlcb.notificacao.Notificador;
 @Component
 public class AtivacaoClienteService {
 
+	@Autowired
 	private Notificador notificador;
 
-	public AtivacaoClienteService(Notificador notificadorEmail) {
-		this.notificador = notificadorEmail;
-	}
+//	public AtivacaoClienteService() {
+//	}
+//	
+//	@Autowired /* essa é a melhor opção */
+//	public AtivacaoClienteService(Notificador notificadorEmail) {
+//		this.notificador = notificadorEmail;
+//	}
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
 	}
 
+//	@Autowired
+//	public void setNotificador(Notificador notificador) {
+//		this.notificador = notificador;
+//	}
+	
 }
